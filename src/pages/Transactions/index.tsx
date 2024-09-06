943102188c342293f0e7552f8dbe8144f7a55da5
Fernando Saraiva <fpsaraiva@gmail.com>
2024-09-06 19:31:20 -0300

    renaming component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,5 @@
 import { Header } from "../../components/Header";
-import { Summary } from "../../components/summary";
+import { Summary } from "../../components/Summary";
 
 import {
   PriceHighlight,
