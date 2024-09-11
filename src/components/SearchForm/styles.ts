8445aef9410ffa9afe1a0c576a6ef80dcf13106c
Fernando Saraiva <fpsaraiva@gmail.com>
2024-09-11 11:08:35 -0300

    making forms work

--- src/components/SearchForm/styles.ts
+++ src/components/SearchForm/styles.ts
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const SearchFormContainer = styled.form`
   display: flex;
   gap: 1rem;
+
   input {
     flex: 1;
     border-radius: 6px;
@@ -14,10 +15,12 @@ export const SearchFormContainer = styled.form`
       color: ${props => props.theme["gray-500"]};
     }
   }
+
   button {
     display: flex;
     align-items: center;
     gap: 0.75rem;
+
     border: 0;
     padding: 1rem;
     background: transparent;
@@ -25,7 +28,14 @@ export const SearchFormContainer = styled.form`
     color: ${props => props.theme["green-300"]};
     font-weight: bold;
     border-radius: 6px;
-    &:hover {
+    cursor: pointer;
+
+    &:disabled {
+      opacity: 0.6;
+      cursor: not-allowed;
+    }
+
+    &:not(:disabled):hover {
       background: ${props => props.theme["green-500"]};
       border-color: ${props => props.theme["green-500"]};
       color: ${props => props.theme["white"]};
